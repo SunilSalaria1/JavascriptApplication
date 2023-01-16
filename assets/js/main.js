0c7daf1f9a19d453746b80718bac9ad1779eed05
Kanchan <kanchandeep.kaur@lpinfotech.com>
2023-01-16 18:26:50 +0530

    Added toggle menu functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /* =================================================== */
-               /* common js file */
+                 /* common js file */
 /* =================================================== */
 
 /* ======= global page loader ======= */
@@ -8,3 +8,11 @@ function loader() {
     document.getElementById("pageLoader").classList.add("d-none");
 }
 setTimeout(loader, 1000);
+
+/* ======= toggle menu ======= */
+
+document.getElementById("aside-toggle-btn").addEventListener('click', menuToggle);
+function menuToggle() {
+    document.querySelector("aside").classList.toggle("aside-hide");
+    document.querySelector("main").classList.toggle("main-full");
+}
