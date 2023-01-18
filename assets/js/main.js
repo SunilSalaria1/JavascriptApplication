9575140d080d6a74c099e24455127f40e8edb266
Kanchan <kanchandeep.kaur@lpinfotech.com>
2023-01-18 13:02:12 +0530

    Worked on post data in register form and clear console errors.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /* =================================================== */
-                /* common js file */
+/* common js file */
 /* =================================================== */
 
 /* ======= global page loader ======= */
@@ -11,33 +11,95 @@ document.onreadystatechange = function () {
 };
 
 /* ======= toggle menu ======= */
-
-document.getElementById("asideToggleBtn").addEventListener('click', menuToggle);
-function menuToggle() {
-    document.querySelector("aside").classList.toggle("aside-hide");
-    document.querySelector("main").classList.toggle("main-full");
+let checkToggle = document.querySelectorAll("asideToggleBtn").length;
+if (checkToggle !== 0) {
+    document.querySelector("#asideToggleBtn").addEventListener('click', menuToggle);
+    function menuToggle() {
+        document.querySelector("aside").classList.toggle("aside-hide");
+        document.querySelector("main").classList.toggle("main-full");
+    }
 }
 
 /* ======= active nav links ======= */
+let checkAside = document.querySelectorAll("aside").length;
+if (checkAside !== 0) {
+    let asideLink = document.querySelector("aside");
+    asideLink.querySelectorAll(".nav-link").forEach(function (link) {
+        if (link.href === window.location.href) {
+            link.classList.add("active");
+        }
+    });
+}
 
-let asideLink = document.querySelector("aside");
-asideLink.querySelectorAll(".nav-link").forEach(function (link) {
-    if (link.href === window.location.href) {
-        link.classList.add("active");
-    }
-});
 
 /* ======= show and hide delete popup ======= */
+let checkDeletePopup = document.querySelectorAll("deletePopup").length;
 
-// show the delete toast
-document.getElementById("deletePopup").addEventListener('click', showdeletePopup);
+if (checkDeletePopup !== 0) {
+    // show the delete toast
+    document.getElementById("deletePopup").addEventListener('click', showdeletePopup);
+    function showdeletePopup() {
+        document.getElementById("deletePopupShow").classList.remove('d-none');
+    }
 
-function showdeletePopup(){
-   document.getElementById('deletePopupShow').classList.remove('d-none');
+    // hide the delete popup
+    document.getElementById("closedeletePopup").addEventListener('click', hidedeletePopup);
+    function hidedeletePopup() {
+        document.getElementById("deletePopupShow").classList.add('d-none');
+    }
+
 }
 
-// hide the delete popup
-document.getElementById("closedeletePopup").addEventListener('click', hidedeletePopup);
-function hidedeletePopup(){
-   document.getElementById('deletePopupShow').classList.add('d-none');
+
+/* ======= post register form data with single pair values ======= */
+
+let checkRegister = document.querySelectorAll("registerForm").length;
+if (checkRegister !== 0) {
+
+    // post register form data with single pair values
+
+    document.getElementById("registerForm").addEventListener('submit', function (event) {
+        event.preventDefault();
+        let userFirstName = document.querySelector('#firstName').value;
+        let userLastName = document.querySelector('#lastName').value;
+        let userEmail = document.querySelector('#email').value;
+        let userProfession = document.querySelector('#profession').value;
+        let userPhoneNumber = document.querySelector('#phoneNumber').value;
+        let userPassword = document.querySelector('#password').value;
+        let userConfirmPassword = document.querySelector('#confirmPassword').value;
+
+        fetch('http://localhost:3004/user', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                firstName: userFirstName,
+                lastName: userLastName,
+                email: userEmail,
+                profession: userProfession,
+                phoneNumber: userPhoneNumber,
+                password: userPassword,
+                confirmPassword: userConfirmPassword
+            })
+        })
+    });
+
+    // post register form data with object
+
+    // document.getElementById("registerForm").addEventListener('submit', function (event) {
+    //     event.preventDefault();
+    //     let registerData = new FormData(this);
+    //     let printData = Object.fromEntries(registerData);
+
+    //     fetch('http://localhost:3004/user', {
+    //         method: 'post',
+    //         headers:{
+    //             'Content-Type':'application/json'
+    //         },
+    //         body: JSON.stringify(printData)
+    //     })
+    // });
 }
+
+
