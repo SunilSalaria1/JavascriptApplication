117918bf4b4baa120068e71717f6edd25a4ffd65
Kanchan <kanchandeep.kaur@lpinfotech.com>
2023-01-19 16:12:33 +0530

    Login form validations.

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,32 +67,35 @@ if (checkRegister !== 0) {
         let userPhoneNumber = document.querySelector('#phoneNumber').value;
         let userPassword = document.querySelector('#password').value;
         let userConfirmPassword = document.querySelector('#confirmPassword').value;
-
-        fetch('http://localhost:3004/user', {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                firstName: userFirstName,
-                lastName: userLastName,
-                email: userEmail,
-                profession: userProfession,
-                phoneNumber: userPhoneNumber,
-                password: userPassword,
-                confirmPassword: userConfirmPassword
+        if (userPassword === userConfirmPassword) {
+            fetch('http://localhost:3004/user', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    firstName: userFirstName,
+                    lastName: userLastName,
+                    email: userEmail,
+                    profession: userProfession,
+                    phoneNumber: userPhoneNumber,
+                    password: userPassword,
+                    confirmPassword: userConfirmPassword
+                })
+            }).then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                console.log('Success:', data);
+                document.getElementById("registerSuccessToast").classList.remove("d-none");
+            }).catch(function (error) {
+                console.log(`Error:`, error);
+                alert("error is");
+                document.getElementById("registerErrorToast").classList.remove("d-none");
             })
-        }).then(function (response) {
-            return response.json();
-        }).then(function (data) {
-            console.log('Success:', data);
-            document.getElementById("registerSuccessToast").classList.remove("d-none");
-            debugger
-        }).catch(function (error) {
-            console.log(`Error:`, error);
-            alert("error is");
-            document.getElementById("registerErrorToast").classList.remove("d-none");
-        })
+        }
+        else {
+            document.getElementById("passwordNotMatched").classList.remove("d-none");
+        }
     });
 
     // post register form data with object
@@ -120,7 +123,7 @@ if (checkTable !== 0) {
             return response.json();
         })
         .then(function (data) {
-            let tableData = " ";
+            let tableData = "";
             data.forEach(function (userData) {
                 tableData += `<tr>
         <th scope="row">${userData.id}</th>
@@ -135,26 +138,64 @@ if (checkTable !== 0) {
             document.getElementById("tableBody").innerHTML = tableData;
         })
 }
- 
-/* ======= get and match login form data ======= */
+
+/* ======= get and match login form data with validations ======= */
 
 let checkLogin = document.querySelectorAll("#loginForm").length;
 if (checkLogin !== 0) {
+
+    // login form submit
     document.getElementById("loginForm").addEventListener('submit', function (event) {
         event.preventDefault();
-        let userLoginEmail = document.querySelector('#loginEmail').value;
-        let userLoginPassword = document.querySelector('#loginPassword').value;
-        fetch('http://localhost:3004/user').then(function (response) {
-            return response.json();
-        }).then(function (data) {
-            for (let i = 0; i < data.length; i++) {
-                if (userLoginEmail == data[i].email && userLoginPassword == data[i].password) {
-                    console.log("successfully logged in");
-                    location.href = "/index.html";
-                    return;
-                }
-            }
-            console.log("data not found");
-        })
-    });
+        let userLoginEmail = document.getElementById("loginEmail").value;
+        let userLoginPassword = document.getElementById("loginPassword").value;
+        if (userLoginEmail === "" || userLoginEmail === null) {
+            document.getElementById("requiredEmailField").innerText = "This is a required field.";
+            document.getElementById("loginEmail").style.borderColor = "red";
+        }
+        if (userLoginPassword === "" || userLoginPassword === null) {
+            document.getElementById("requiredPasswordField").innerText = "This is a required field.";
+            document.getElementById("loginPassword").style.borderColor = "red";
+        }
+        if (userLoginEmail.length > 0 && userLoginPassword.length > 0) {
+            let validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            if(userLoginEmail.match(validEmail)){
+                document.getElementById("requiredEmailField").innerText="";
+                document.getElementById("requiredPasswordField").innerText="";
+                if (userLoginEmail.includes("@")) {
+                    console.log("contains");
+                } 
+                fetch('http://localhost:3004/user').then(function (response) {
+                    return response.json();
+                }).then(function (data) {
+                    for (let i = 0; i < data.length; i++) {
+                        if (userLoginEmail == data[i].email && userLoginPassword == data[i].password) {
+                            document.getElementById("loginSuccessToast").classList.replace("hide", "show");
+                            setTimeout(function () {
+                                window.location.href = "/index.html"
+                            }, 1000)
+                            return;
+                        }
+                    }
+                    document.getElementById("loginErrorToast").classList.replace("hide", "show");
+                })
+            }  
+            else{
+                document.getElementById("requiredEmailField").innerText = "Please include @ in the email address."; 
+            }       
+        }
+    }
+    );
+
+    // input email keydown
+    document.getElementById("loginEmail").addEventListener('keydown', function(){
+        document.getElementById("requiredEmailField").innerText = "";
+        document.getElementById("loginEmail").style.borderColor = "#ced4da";
+    })
+
+    // input password keydown
+    document.getElementById("loginPassword").addEventListener('keydown', function(){
+        document.getElementById("requiredPasswordField").innerText = "";
+        document.getElementById("loginPassword").style.borderColor = "#ced4da";
+    })
 }
